bbfd59566096b659c290f84e86d0e567817c1d8c
ellmo <ellmunzai@gmail.com>
2011-02-19 21:26:48 +0100

    Modified the starmap controller so it would use the Rafagg class properly

--- app/controllers/starmap_controller.rb
+++ app/controllers/starmap_controller.rb
@@ -9,28 +9,14 @@ class StarmapController < ApplicationController
     @canvas_size = 640
     @number_of_planets = 12
     @orbit_step = ((@canvas_size-20)/@number_of_planets)/2.to_f
+    orbit_params = {'stroke' => 'white', 'stroke-dasharray' => "-", 'stroke-opacity' => 0.6}
+    sun_params = {'stroke' => 'none', 'fill' => 'yellow'}
 
-    ruafa = EllmoRafaWrapper::Ruafa.new("mapcanvas", @canvas_size, @canvas_size)
-    ruafa.render
-    #render :js => "var rafa = Raphael('mapcanvas', #{@canvas_size}, #{@canvas_size});"
-
-=begin
-    render :text =>
-    canvas('mapcanvas', @canvas_size, @canvas_size) do |canvas|
-
-      orbit_params = {'stroke' => 'white', 'stroke-dasharray' => ". ", 'stroke-opacity' => 0.6}
-      sun_params = {'stroke' => 'none', 'fill' => 'yellow'}
-
-      1.upto(@number_of_planets) { |x|
-        canvas.circle(
-          @canvas_size/2,
-          @canvas_size/2,
-          x * @orbit_step,
-          orbit_params
-        )
-      }
-      #canvas.circle(50, 50, 20, params)
+    r = Rafagg.new("mapcanvas", @canvas_size, @canvas_size)
+    1.upto(@number_of_planets) do |x|
+      r.circle("planet#{x}", @canvas_size/2, @canvas_size/2, x * @orbit_step, orbit_params)
     end
-=end
+
+    render :js => r.call
   end
 end
--- lib/rafa_agg.rb
+++ lib/rafa_agg.rb
@@ -9,25 +9,26 @@ module RafaAggregate
     # javascript calls in order.
     # Arguments are:
     # * element_id:String - the html id of an element (a div would actually be the only reasonable choice) to start the Raphael canvas in
-    # * width:Fixnum - width of the canvas object, it can be different than the html element's width, but it really shouldn't
-    # * height:Fixnum - height of the canvas object
+    # * width:Numeric - width of the canvas object, it can be different than the html element's width, but it really shouldn't
+    # * height:Numeric - height of the canvas object
     def initialize(element_id, width, height)
       (element_id.is_a? String) ?
         @element_id = element_id : raise(ArgumentError, "element_id has to be a String")
-      (width.is_a? Fixnum) ?
-        @width = width : raise(ArgumentError, "width has to be a Fixnum")
-      (height.is_a? Fixnum) ?
-        @height = height : raise(ArgumentError, "height has to be a Fixnum");
-      @reg = ["var raphael = Raphael('#{@element_id}', #{@width}, #{@height});"]
+      (width.is_a? Numeric) ?
+        @width = width : raise(ArgumentError, "width has to be a Numeric")
+      (height.is_a? Numeric) ?
+        @height = height : raise(ArgumentError, "height has to be a Numeric");
+      @reg = ["var raphael = Raphael(\"#{@element_id}\", #{@width}, #{@height});"]
       @backup_reg = false
       @reg_old = []
     end
 
     # Releases the string registry, so that the string output
     # can be rendered using 'render :js => Rafagg.call' method.
-    # Current registry is emptied in the process and it's contents
-    # are added to the @reg_old array (should you need them while
-    # debugging)
+    # Current registry is emptied in the process. If 'backup_reg'
+    # value is true, the contents of the current registry are
+    # added to the @reg_old array.
+    # (should you need them while debugging)
     def call
       _reg = @reg
       @reg_old += @reg if @backup_reg
@@ -44,26 +45,26 @@ module RafaAggregate
     # parameters through Raphael.js to the class registry.
     # The method accepts 4 or 5 arguments:
     # * name:String - the name of the object created
-    # * cx:Fixnum - x position of the circle
-    # * cy:Fixnum - y position of the circle
-    # * radius:Fixnum - radius of the circle
+    # * cx:Numeric - x position of the circle
+    # * cy:Numeric - y position of the circle
+    # * radius:Numeric - radius of the circle
     # * params:Hash - optional collection of SVG attributes (e.g. {"stroke-dasharray" => "-."})
     def circle(name, cx, cy, radius, params=nil)
-      if( name.is_a? String and cx.is_a? Fixnum and cy.is_a? Fixnum and radius.is_a? Fixnum )
+      if( name.is_a? String and cx.is_a? Numeric and cy.is_a? Numeric and radius.is_a? Numeric )
         @reg.push("var #{name} = raphael.circle(#{cx}, #{cy}, #{radius});")
         set_attr(name, params) if (params and params.is_a? Hash)
       else
-        raise(ArgumentError, "The arguments are: object's name, center x, center y, radius, SVG parameters")
+        raise(ArgumentError, "The arguments are: object`s name, center x, center y, radius, SVG parameters")
       end
     end
     
-    # Modifies SVG attributes of a given Raphael object
+    # Modifies SVG attributes of a given Raphael object.
     # Arguments are:
     # * name:String - the name of a Raphael object to be modified
     # * params:Hash - collection of SVG attributes (e.g. {"stroke-dasharray" => "-."})
     def set_attr(name, params)
       if( name.is_a? String and params.is_a? Hash)
-        _params = name + ".attr({" + (params.collect {|x| "'#{x[0]}':'#{x[1]}', "}).to_s + "});"
+        _params = name + ".attr({" + (params.collect {|x| "\"#{x[0]}\":\"#{x[1]}\", "}).to_s + "});"
         @reg.push _params
       else
         raise(ArgumentError, "The arguments are: object's name, SVG parameters")
